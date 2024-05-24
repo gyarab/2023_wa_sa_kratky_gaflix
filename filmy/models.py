47adca1e47e0736883ffe1effbd022139dea4d51
Martin Kratky <martin.kratky@student.gyarab.cz>
2024-05-24 11:20:16 +0200

    fixtures

--- filmy/models.py
+++ filmy/models.py
@@ -8,7 +8,7 @@ class Movie(models.Model):
         blank=True, null=True, help_text="in minutes"
     )
     description = models.TextField(blank=True)
-    main_picture = models.CharField(max_length=500, null=True)
+    main_picture = models.CharField(max_length=2000, null=True)
     director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
     actors = models.ManyToManyField('Actor', blank=True, null=True)
     genres = models.ManyToManyField('Genre', blank=True, null=True)
@@ -24,7 +24,7 @@ class Director(models.Model):
     name = models.CharField(max_length=300, null=True)
     birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    main_picture = models.CharField(max_length=500, null=True)
+    main_picture = models.CharField(max_length=2000, null=True)
 
     def __str__(self):
         return self.name
@@ -41,7 +41,7 @@ class Actor(models.Model):
     name = models.CharField(max_length=300, null=True)
     birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
     description = models.TextField(blank=True)
-    main_picture = models.CharField(max_length=500, null=True)
+    main_picture = models.CharField(max_length=2000, null=True)
 
     def __str__(self):
         return self.name
