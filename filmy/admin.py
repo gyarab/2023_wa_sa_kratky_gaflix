e2987b1b96bbdd1fd6e8e732c4d675e9b5c60c3f
Martin Kratky <martin.kratky@student.gyarab.cz>
2024-05-24 09:58:13 +0200

    Gaflix

--- filmy/admin.py
+++ filmy/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 from filmy.models import Movie, Director, Genre, Actor
 
+
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+
+class DirectorAdmin(admin.ModelAdmin):
+    pass
+
+
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+class ActorAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Movie)
 admin.site.register(Director)
 admin.site.register(Genre)
--- filmy/models.py
+++ filmy/models.py
@@ -1,47 +1,41 @@
 from django.db import models
 
-# Create your models here.
 
 class Movie(models.Model):
-    name = models.CharField(max_length=500)
-    year = models.IntegerField(blank=True, null=True)
-    footage = models.PositiveSmallIntegerField(blank=True, null=True)
+    name = models.CharField(max_length=300, null=True)
+    year = models.PositiveSmallIntegerField(blank=True, null=True)
+    footage = models.PositiveSmallIntegerField(
+        blank=True, null=True, help_text="in minutes"
+    )
+    description = models.TextField(blank=True)
+    main_picture = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.name
-    
-    class Meta:
-        verbose_name = 'Film'
-        verbose_name_plural = 'Filmy'
+
 
 class Director(models.Model):
-    name = models.CharField(max_length=500)
+    name = models.CharField(max_length=300, null=True)
     birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+    main_picture = models.ImageField(blank=True, null=True)
 
     def __str__(self):
-        return f'{self.name}'
-    
-    class Meta:
-        verbose_name = 'Director'
-        verbose_name_plural = 'Directors'
+        return self.name
+
 
 class Genre(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=300, null=True)
 
     def __str__(self):
         return self.name
-    
-    class Meta:
-        verbose_name = 'Genre'
-        verbose_name_plural = 'Genres'
+
 
 class Actor(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=300, null=True)
+    birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True)
+    main_picture = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.name
-    
-    class Meta:
-        verbose_name = 'Actor'
-        verbose_name_plural = 'Actor'
