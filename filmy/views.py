125fda672f2f089d7e7cd5d470d1b2c82e373c5b
Martin Kratky <martin.kratky@student.gyarab.cz>
2024-05-31 11:21:03 +0200

    frontend

--- filmy/views.py
+++ filmy/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+from filmy.models import Movie
+
 def index(request):
     
     return render(request, 'filmy/base.html')
+
+def movies(register):
+
+    context = {
+        'movies': Movie.objects.all().order_by('name')
+    }
+
+    return render(register, 'filmy/movies.html', context)
+
+def movie(request, id):
+    
+    context = {
+        'movie': Movie.objects.get(id=id)
+    }
+
+    return render(register, 'filmy/movie.html', context)
--- gaflix/urls.py
+++ gaflix/urls.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from filmy.views import movies, movie
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('filmy/', include('filmy.urls'), name='filmy'),
     path('', TemplateView.as_view(template_name='filmy/prvni.html'), name='home'),
     path('druha/', TemplateView.as_view(template_name='filmy/druha.html'), name='druha'),
+    path('movies/', movies, name="movies"),
+    path('movie/<int:id>', movie, name="movie"),
 ]
