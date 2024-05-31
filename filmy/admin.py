ef839726aefe4587a065f25efa2b05c1118be9a0
Martin Kratky <martin.kratky@student.gyarab.cz>
2024-05-31 09:56:13 +0200

    fixtures

--- filmy/admin.py
+++ filmy/admin.py
@@ -5,25 +5,25 @@ from filmy.models import Movie, Director, Genre, Actor
 
 class MovieAdmin(admin.ModelAdmin):
     
-    list_display = ["id", "name", "year", "footage", "director", "genres_display"]
+    list_display = ["id", "name", "year", "footage", "director", "genres_display", "main_picture"]
     list_display_links = ["id", "name"]
     search_fields = ["=id", "name", "director__name"]
     list_filter = ["year", "genres"]
-    list_editable = ["year", "footage"]
+    list_editable = ["year", "footage", "main_picture"]
 
 
 class DirectorAdmin(admin.ModelAdmin):
     
     list_display = ["id", "name", "birth_year", "description", "main_picture"]
-    list_display_links = ["id", "name", "birth_year"]
+    list_display_links = ["id", "birth_year"]
     search_fields = ["=id", "name", "birth_year"]
-    list_editable = ["name", "description"]
+    list_editable = ["name", "description", "main_picture"]
 
 
 class GenreAdmin(admin.ModelAdmin):
     
     list_display = ["id", "name"]
-    list_display_links = ["id", "name"]
+    list_display_links = ["id"]
     search_fields = ["=id", "name"]
     list_editable = ["name"]
 
@@ -31,7 +31,7 @@ class GenreAdmin(admin.ModelAdmin):
 class ActorAdmin(admin.ModelAdmin):
     
     list_display = ["id", "name", "birth_year", "description", "main_picture"]
-    list_display_links = ["id", "name", "birth_year"]
+    list_display_links = ["id", "birth_year"]
     search_fields = ["=id", "name", "birth_year"]
     list_editable = ["name", "description",  "main_picture"]
 
