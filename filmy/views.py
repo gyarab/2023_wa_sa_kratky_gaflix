8fd576bce7f2029e8976e8982235788ed95da8ae
Martin Kratky <martin.kratky@student.gyarab.cz>
2024-06-12 20:01:54 +0200

    frontend

--- filmy/views.py
+++ filmy/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 
-from filmy.models import Movie
+from filmy.models import Movie, Actor, Director
 
 def index(request):
     
     return render(request, 'filmy/base.html')
 
-def movies(register):
+def movies(request):
 
     context = {
         'movies': Movie.objects.all().order_by('name')
     }
 
-    return render(register, 'filmy/movies.html', context)
+    return render(request, 'filmy/movies.html', context)
 
 def movie(request, id):
     
@@ -20,4 +20,37 @@ def movie(request, id):
         'movie': Movie.objects.get(id=id)
     }
 
-    return render(register, 'filmy/movie.html', context)
+    return render(request, 'filmy/movie.html', context)
+
+def actors(request):
+
+    context = {
+        'actors': Actor.objects.all().order_by('name')
+    }
+
+    return render(request, 'filmy/actors.html', context)
+
+def actor(request, id):
+    
+    context = {
+        'actor': Actor.objects.get(id=id)
+    }
+
+    return render(request, 'filmy/actor.html', context)
+
+
+def directors(request):
+
+    context = {
+        'directors': Director.objects.all().order_by('name')
+    }
+
+    return render(request, 'filmy/directors.html', context)
+
+def director(request, id):
+    
+    context = {
+        'director': Director.objects.get(id=id)
+    }
+
+    return render(request, 'filmy/director.html', context)
--- gaflix/urls.py
+++ gaflix/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from filmy.views import movies, movie
+from filmy.views import movies, movie, actor, actors, director, directors
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +11,8 @@ urlpatterns = [
     path('druha/', TemplateView.as_view(template_name='filmy/druha.html'), name='druha'),
     path('movies/', movies, name="movies"),
     path('movie/<int:id>', movie, name="movie"),
+    path('actors/', actors, name="actors"),
+    path('actor/<int:id>', actor, name="actor"),
+    path('directors/', directors, name="directors"),
+    path('director/<int:id>', director, name="director"),
 ]
